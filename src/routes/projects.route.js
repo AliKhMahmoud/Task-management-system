077fe0b8b7952926f86a7d5c30d3bd0bf647135a
unknown <georgenasser2002@gmail.com>
2026-01-03 21:24:13 -0800

    Implement project management features with role-based access control

--- src/routes/projects.route.js
+++ src/routes/projects.route.js
@@ -1,61 +1,52 @@
-const express = require('express');
+const express = require("express");
 const projectController = require("../controllers/projects.controller");
-const asyncHandler = require('../utils/asyncHandler');
-const { apiLimiter } = require('../middlewares/rateLimit.middleware');
+const asyncHandler = require("../utils/asyncHandler");
+const { apiLimiter } = require("../middlewares/rateLimit.middleware");
 
 const router = express.Router();
 
-// TODO: Add note routes here
-// Example:
+// Projects CRUD
+router.post(
+  "/",
+  [apiLimiter],
+  asyncHandler(projectController.createProjectByManager)
+);
 
-// router.post('/',
-//     [
-//         apiLimiter
-//     ],
-//     asyncHandler(projectController.createProjectByManager)
-// );
+router.get(
+  "/",
+  [apiLimiter],
+  asyncHandler(projectController.getAllProjects)
+);
 
-// router.get('/',
-//     [
-//         apiLimiter
-//     ],
-//     asyncHandler(projectController.getAllProjects)
-// );
+router.get(
+  "/:id",
+  [apiLimiter],
+  asyncHandler(projectController.getProjectById)
+);
 
-// router.get('/:id',
-//     [
-//         apiLimiter
-//     ],
-//     asyncHandler(projectController.getProjectById)
-// );
+router.put(
+  "/:id",
+  [apiLimiter],
+  asyncHandler(projectController.updateProjectByManager)
+);
 
-// router.put('/:id',
-//     [
-//         apiLimiter
-//     ],
-//     asyncHandler(projectController.updateProjectByManager)
-// );
+router.delete(
+  "/:id",
+  [apiLimiter],
+  asyncHandler(projectController.removeProjectByManager)
+);
 
-// router.delete('/:id',
-//     [
-//         apiLimiter
-//     ],
-//     asyncHandler(projectController.removeProjectByManager)
-// );
+// Members management
+router.post(
+  "/:id/members",
+  [apiLimiter],
+  asyncHandler(projectController.addMemberByManager)
+);
 
-// router.post('/addMember',
-//     [
-//         apiLimiter
-//     ],
-//     asyncHandler(projectController.addMemberByManager)
-// );
-
-// router.delete('/addMember',
-//     [
-//         apiLimiter
-//     ],
-//     asyncHandler(projectController.removeMemberByManager)
-// );
-
+router.delete(
+  "/:id/members/:memberId",
+  [apiLimiter],
+  asyncHandler(projectController.removeMemberByManager)
+);
 
 module.exports = router;
